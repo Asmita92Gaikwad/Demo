c77c8521757b8d1057796c5fdabb48f639bb00cc
Asmita Gaikwad <64396544+Asmita92Gaikwad@users.noreply.github.com>
2020-04-28 15:34:10 +0530

    lines added

---
+++ main.py
@@ -0,0 +1,11 @@
+#global variables
+#game board
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-",]
+
+#if game is still going
+game_stile_going = True
+
+#who won or tie?
+winner = None
